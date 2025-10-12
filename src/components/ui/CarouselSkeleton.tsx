7245cb49c67ae859c3ed9037a5a03ea02d8aebda
DESKTOP-SPQ41UM\user <jiyeyoun@outlook.com>
2025-10-12 21:46:45 +0900

    chore(ui): 자잘한 ui 요소 변경

--- src/components/ui/CarouselSkeleton.tsx
+++ src/components/ui/CarouselSkeleton.tsx
@@ -1,12 +1,30 @@
-import { Carousel } from '@/components/common';
-import { CardSkeleton } from './CardSkeleton';
+import useEmblaCarousel from "embla-carousel-react";
 
-export function CarouselSkeleton({ count = 8 }: { count?: number }) {
+export function CarouselSkeleton() {
+  const [emblaRef] = useEmblaCarousel({
+    loop: true,
+    align: 'start',
+    dragFree: true,
+    containScroll: 'trimSnaps',
+  });
   return (
-    <Carousel
-      items={Array.from({ length: count }).map((_, i) => (
-        <CardSkeleton key={i} />
-      ))}
-    />
+    <div className='relative mx-4'>
+      <div className='overflow-hidden' ref={emblaRef}>
+        <div className='embla__container flex gap-4'>
+        <div className='centralize relative aspect-video h-full w-full rounded-md p-4 transition-opacity duration-300 ease-in-out'>
+        <figure className='aspect-video w-full overflow-hidden rounded-md'>
+          <img
+            className='absolute inset-0 h-full w-full rounded-md object-cover object-center bg-zinc-300 animate-pulse'
+          />
+          <div className='absolute inset-0 rounded-md bg-black/30' />
+          <div className='absolute bottom-6 left-6 pr-6'>
+            <h2 className='text-xl font-bold sm:text-3xl md:text-2xl bg-zinc-300 animate-pulse'></h2>
+            <p className='mt-1 line-clamp-1 text-sm opacity-80 sm:line-clamp-2 bg-zinc-300 animate-pulse'></p>
+          </div>
+        </figure>
+      </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/features/movie/components/MovieCarousel.tsx
+++ src/features/movie/components/MovieCarousel.tsx
@@ -17,12 +17,9 @@ export default function MovieCarousel({ movies }: MovieCarouselProps) {
   return (
     <div className='relative mx-4'>
       <div className='overflow-hidden' ref={emblaRef}>
-        <div className='flex gap-4'>
+        <div className='embla__container flex gap-4'>
           {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className='embla__slide relative flex-[0_0_100%] flex-shrink-0 flex-grow-0 px-2'
-            >
+            <div key={movie.id} className='embla__slide relative flex-shrink-0 flex-grow-0 px-2'>
               <MovieCarouselCard key={movie.id} movie={movie} />
             </div>
           ))}
